b9046a13044d2500ee983024e5e66bbbfd8d665c
zhouhao <zh.sqy@qq.com>
2024-07-19 10:47:47 +0800

    refactor: 优化命令

--- plugin-core/src/main/java/org/jetlinks/plugin/core/command/GetConfigMetadataCommand.java
+++ plugin-core/src/main/java/org/jetlinks/plugin/core/command/GetConfigMetadataCommand.java
@@ -1,7 +1,10 @@
 package org.jetlinks.plugin.core.command;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.jetlinks.core.command.AbstractCommand;
+import org.jetlinks.core.command.CommandMetadataResolver;
 import org.jetlinks.core.metadata.ConfigMetadata;
+import org.jetlinks.core.metadata.FunctionMetadata;
 import org.jetlinks.plugin.core.PluginDriver;
 import reactor.core.publisher.Mono;
 
@@ -10,5 +13,12 @@ import reactor.core.publisher.Mono;
  *
  * @see PluginDriver#getDescription()
  */
+@Schema(title = "获取配置信息")
 public class GetConfigMetadataCommand extends AbstractCommand<Mono<ConfigMetadata>, GetConfigMetadataCommand> {
+
+
+    public static FunctionMetadata metadata(){
+        return CommandMetadataResolver.resolve(GetConfigMetadataCommand.class);
+    }
+
 }
