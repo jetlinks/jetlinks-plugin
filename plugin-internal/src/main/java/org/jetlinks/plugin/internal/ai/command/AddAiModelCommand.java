89e0055b9d387b42710ba39cc72779f958746bf2
gyl <80454341+goten7@users.noreply.github.com>
2024-09-13 17:22:21 +0800

    feat: 添加ai模型相关命令 (#11)
    * feat: 添加ai模型相关命令
    
    * feat: 优化代码

---
+++ plugin-internal/src/main/java/org/jetlinks/plugin/internal/ai/command/AddAiModelCommand.java
@@ -0,0 +1,29 @@
+package org.jetlinks.plugin.internal.ai.command;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.hswebframework.web.bean.FastBeanCopier;
+import org.jetlinks.core.command.AbstractConvertCommand;
+import org.jetlinks.core.command.CommandMetadataResolver;
+import org.jetlinks.core.metadata.FunctionMetadata;
+import org.jetlinks.sdk.server.ai.model.AiModelInfo;
+import org.jetlinks.sdk.server.ai.model.AiModelPortrait;
+import reactor.core.publisher.Mono;
+
+
+@Schema(title = "添加AI模型")
+public class AddAiModelCommand extends AbstractConvertCommand<Mono<AiModelPortrait>, AddAiModelCommand> {
+
+    @Schema(title = "模型信息")
+    public AiModelInfo getModel() {
+        Object val = this.readable().get("model");
+        return val instanceof AiModelInfo ? ((AiModelInfo) val) : FastBeanCopier.copy(val, new AiModelInfo());
+    }
+
+    public void setModel(AiModelInfo model) {
+        this.readable().put("model", model);
+    }
+
+    public static FunctionMetadata metadata() {
+        return CommandMetadataResolver.resolve(AddAiModelCommand.class);
+    }
+}
