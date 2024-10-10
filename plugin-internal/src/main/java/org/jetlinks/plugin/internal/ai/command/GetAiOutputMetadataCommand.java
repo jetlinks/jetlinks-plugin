142153378b878da15cdac5f62343c711dcbec5aa
gyl <1554863190@qq.com>
2024-10-10 20:28:17 +0800

    refactor: 优化

--- plugin-internal/src/main/java/org/jetlinks/plugin/internal/ai/command/GetAiOutputMetadataCommand.java
+++ plugin-internal/src/main/java/org/jetlinks/plugin/internal/ai/command/GetAiOutputMetadataCommand.java
@@ -3,6 +3,7 @@ package org.jetlinks.plugin.internal.ai.command;
 import com.google.common.collect.Lists;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.jetlinks.core.command.CommandHandler;
+import org.jetlinks.core.command.CommandMetadataResolver;
 import org.jetlinks.core.command.CommandUtils;
 import org.jetlinks.core.metadata.PropertyMetadata;
 import org.jetlinks.core.metadata.SimpleFunctionMetadata;
@@ -10,6 +11,7 @@ import org.jetlinks.core.metadata.SimplePropertyMetadata;
 import org.jetlinks.core.metadata.types.StringType;
 import org.jetlinks.sdk.server.ai.AiOutputMetadataType;
 import org.jetlinks.sdk.server.commons.cmd.OperationByIdCommand;
+import org.jetlinks.sdk.server.utils.ConverterUtils;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -20,12 +22,13 @@ import java.util.function.Function;
  * @author gyl
  * @since 2.3
  */
+@Schema(title = "获取ai输出指定类型配置定义")
 public class GetAiOutputMetadataCommand extends OperationByIdCommand<Mono<Map</*命令id*/String, List<PropertyMetadata>>>, GetAiOutputMetadataCommand> {
 
 
     @Schema(title = "输出类型")
     public AiOutputMetadataType getType() {
-        return AiOutputMetadataType.valueOf((String) readable().get("type"));
+        return AiOutputMetadataType.valueOf(String.valueOf(readable().get("type")));
     }
 
     public void setType(AiOutputMetadataType type) {
@@ -34,16 +37,8 @@ public class GetAiOutputMetadataCommand extends OperationByIdCommand<Mono<Map</*
 
     public static CommandHandler<GetAiOutputMetadataCommand, Mono<Map</*命令id*/String, List<PropertyMetadata>>>> createHandler
         (Function<GetAiOutputMetadataCommand, Mono<Map</*命令id*/String, List<PropertyMetadata>>>> handler) {
-        SimpleFunctionMetadata metadata = new SimpleFunctionMetadata();
-        metadata.setId(CommandUtils.getCommandIdByType(GetAiOutputMetadataCommand.class));
-        metadata.setName("获取ai输出指定类型配置定义");
-        metadata.setInputs(Lists.newArrayList(
-            SimplePropertyMetadata.of("id", "命令Id，为空时获取所有", StringType.GLOBAL),
-            SimplePropertyMetadata.of("type", "输出类型", StringType.GLOBAL)
-
-        ));
         return CommandHandler
-            .of(metadata,
+            .of(CommandMetadataResolver.resolve(GetAiOutputMetadataCommand.class),
                 (req, spt) -> handler.apply(req),
                 GetAiOutputMetadataCommand::new);
     }
